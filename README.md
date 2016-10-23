d34e72dfe8abf82ef9be00ba5c9398ac0d26516d
Jon Tai <jon.tai@gmail.com>
2016-10-23 16:43:29 -0700

    Add README, skill, and supporting files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # alexa-pagerduty
-PagerDuty Alexa skill that uses the PagerDuty calendar feed
+Unofficial PagerDuty Alexa skill that uses the PagerDuty calendar feed
+
+_Disclaimer: I am not affiliated with PagerDuty in any way._
+
+## Setup
+
+1. [Register](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/registering-and-managing-alexa-skills-in-the-developer-portal) a new Alexa skill, noting the `applicationId`. Copy and paste the Intent Schema from `intents.json` and Sample Utterances from `utterances.txt`.
+2. Log into PagerDuty, go to your Profile, and find the link to your iCalendar file under the User Settings tab.
+3. Edit `package.json` to set `alexa.applicationId` and `pagerDuty.url`.
+4. Run `npm install`.
+5. Zip up `index.js`, `package.json`, and the `node_modules` directory.
+6. [Create](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/developing-an-alexa-skill-as-a-lambda-function) an AWS Lambda function and upload the zip file.
---
+++ index.js
@@ -0,0 +1,233 @@
+var alexa = require('alexa-app');
+var https = require('https');
+var moment = require('moment-timezone');
+var humanizeList = require('humanize-list');
+var package_json = require('./package.json');
+
+// Allow this module to be reloaded by hotswap when changed
+module.change_code = 1;
+
+var getSchedules = function(success, error) {
+  getCalendarFeed(function(data) {
+    var timezone = parseCalendarFeedTimezone(data);
+    var events = parseCalendarFeedEvents(data);
+    var now = moment();
+    var activeSchedules = getActiveSchedules(events, now);
+    var nextSchedules = getNextSchedules(events, now);
+
+    success({
+      timezone: timezone,
+      now: now,
+      activeSchedules: activeSchedules,
+      nextSchedules: nextSchedules
+    });
+  }, error);
+};
+
+var getCalendarFeed = function(success, error) {
+  https.get(package_json.pagerduty.url, function(response) {
+    if (response.statusCode == 200) {
+      var body = '';
+      response.on('data', function(data) {
+        body += data;
+      });
+      response.on('end', function() {
+        success(body);
+      });
+    } else {
+      error(response);
+    }
+  }).on('error', function(e) {
+    error(e);
+  });
+};
+
+var parseCalendarFeedTimezone = function(data) {
+  var timezone = 'UTC';
+  var flag = false;
+
+  data.split("\n").forEach(function(line) {
+    if (line == 'BEGIN:VTIMEZONE') {
+      flag = true;
+    } else if (line == 'END:VTIMEZONE') {
+      flag = false;
+    } else if (flag) {
+      var kv = line.split(':');
+      var key = kv[0].split(';')[0];
+      var value = kv[1];
+      if (key == 'TZID') {
+        timezone = value;
+      }
+    }
+  });
+
+  return timezone;
+};
+
+var parseCalendarFeedEvents = function(data) {
+  var events = [];
+  var vevent = {};
+
+  data.split("\n").forEach(function(line) {
+    if (line == 'BEGIN:VEVENT') {
+      vevent = {};
+    } else if (line == 'END:VEVENT') {
+      events.push(vevent);
+      vevent = {};
+    } else {
+      var kv = line.split(':');
+      var key = kv[0].split(';')[0];
+      var value = kv[1];
+      if (key == 'SUMMARY') {
+        value = value.replace(/^On Call - /, '');
+      }
+      vevent[key] = value;
+    }
+  });
+
+  return events;
+};
+
+var getActiveSchedules = function(events, now) {
+  return events.filter(function(vevent) {
+    return now.isBetween(vevent.DTSTART, vevent.DTEND);
+  });
+};
+
+var getNextSchedules = function(events, now) {
+  var futureSchedules = events.filter(function(vevent) {
+    return now.isBefore(vevent.DTSTART);
+  });
+
+  var earliestSchedule = futureSchedules.reduce(function(a, b) {
+    return moment(a.DTSTART).isBefore(b.DTSTART) ? a : b;
+  });
+  var earliestDtstart = moment(earliestSchedule.DTSTART);
+
+  return futureSchedules.filter(function(vevent) {
+    return earliestDtstart.isSame(vevent.DTSTART);
+  });
+};
+
+var humanizeSchedules = function(events) {
+  return humanizeList(events.map(function(vevent) {
+    return vevent.SUMMARY;
+  }));
+};
+
+var humanizeSchedulesWithEndTimes = function(events, timezone, now) {
+  var endTimes = {};
+  events.forEach(function(vevent) {
+    if (!endTimes[vevent.DTEND]) {
+      endTimes[vevent.DTEND] = [];
+    }
+    endTimes[vevent.DTEND].push(vevent.SUMMARY);
+  });
+
+  var list = [];
+  for (var endTime in endTimes) {
+    if (!endTimes.hasOwnProperty(endTime)) {
+        continue;
+    }
+    list.push(humanizeList(endTimes[endTime])+" until "+humanizeTime(endTime, timezone, now));
+  }
+
+  return humanizeList(list);
+};
+
+var humanizeTime = function(time, timezone, now) {
+  return moment(time).tz(timezone).calendar(now, {sameElse: 'dddd, MMMM Do [at] LT'});
+};
+
+var app = new alexa.app('pagerduty');
+
+app.pre = function(request,response,type) {
+  if (request.sessionDetails.application.applicationId != package_json.alexa.applicationId) {
+    // Fail ungracefully
+    response.fail("Invalid applicationId");
+  }
+};
+
+app.launch(function(request, response) {
+  response.say("You can ask if you are on call, what schedule you are on call for, or when " +
+               "your current on call ends. You can also ask when your next on call starts.");
+});
+
+app.intent('IsOnCallIntent', {
+  utterances: [
+    "if I am {|currently }on call",
+    "if I am on call right now",
+    "am I {|currently }on call",
+    "am I on call right now",
+  ]
+}, function(request, response) {
+  getSchedules(function(data) {
+    if (data.activeSchedules.length == 0) {
+      response.say("No. You will be on call for "+humanizeSchedules(data.nextSchedules)+
+                   " starting "+humanizeTime(data.nextSchedules[0].DTSTART, data.timezone, data.now)+".");
+    } else {
+      response.say("Yes. You are on call for "+humanizeSchedulesWithEndTimes(data.activeSchedules, data.timezone, data.now)+".");
+    }
+    response.send();
+  }, function(error) {
+    response.say("Sorry, I'm having trouble getting the pager duty schedule.");
+    response.send();
+  });
+
+  // Return false immediately so alexa-app doesn't send the response
+  return false;
+});
+
+app.intent('ActiveSchedulesIntent', {
+  utterances: [
+    "what {|rotation |rotations |schedule |schedules |policy |policies }am I {|currently }on call for",
+    "what {|rotation |rotations |schedule |schedules |policy |policies }am I on call for right now",
+    "when my {|current }on call {|rotation |schedule |policy }ends",
+    "when does my {|current }on call {|rotation |schedule |policy }end",
+    "when is my {|current }on call {|rotation |schedule |policy }over",
+  ]
+}, function(request, response) {
+  getSchedules(function(data) {
+    if (data.activeSchedules.length == 0) {
+      response.say("You are not currently on call. You will be on call for "+humanizeSchedules(data.nextSchedules)+
+                   " starting "+humanizeTime(data.nextSchedules[0].DTSTART, data.timezone, data.now)+".");
+    } else {
+      response.say("You are on call for "+humanizeSchedulesWithEndTimes(data.activeSchedules, data.timezone, data.now)+".");
+    }
+    response.send();
+  }, function(error) {
+    response.say("Sorry, I'm having trouble getting the pager duty schedule.");
+    response.send();
+  });
+
+  // Return false immediately so alexa-app doesn't send the response
+  return false;
+});
+
+app.intent('NextSchedulesIntent', {
+  utterances: [
+    "what {|rotation |rotations |schedule |schedules |policy |policies }am I on call for next",
+    "when does my {|next }on call {|rotation |schedule |policy }{start|begin}",
+    "when is my {|next }on call {|rotation |schedule |policy }",
+    "when am I next on call",
+    "when am I on call next",
+  ]
+}, function(request, response) {
+  getSchedules(function(data) {
+    if (data.nextSchedules.length == 0) {
+      response.say("You are not scheduled for any upcoming on call.");
+    } else {
+      response.say("You will be on call for "+humanizeSchedules(data.nextSchedules)+
+                   " starting "+humanizeTime(data.nextSchedules[0].DTSTART, data.timezone, data.now)+".");
+    }
+    response.send();
+  }, function(error) {
+    response.say("Sorry, I'm having trouble getting the pager duty schedule.");
+    response.send();
+  });
+
+  // Return false immediately so alexa-app doesn't send the response
+  return false;
+});
+
+module.exports = app;
